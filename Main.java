854a9fae3985f1d7394d388365651deec7f1dae9
Bekas <igomorev@bk.ru>
2022-08-14 18:39:53 +0300

    Версия

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean myBool = true;
+        System.out.println(myBool);
+        byte myB = 127;
+        System.out.println(myB);
+    }
+}
